6dfe1ae19e8bf13a7c53f4fbc9d8ab43819e9497
SOSA <SOSA@DESKTOP-V4PQRV0>
2024-06-26 20:26:14 -0300

    Modificando el model Docente para agregar las anotaciones de Lombok

--- tp4_VelizGuadalupe/src/main/java/ar/edu/unju/fi/model/Docente.java
+++ tp4_VelizGuadalupe/src/main/java/ar/edu/unju/fi/model/Docente.java
@@ -1,25 +1,40 @@
 package ar.edu.unju.fi.model;
 
+
 import org.springframework.stereotype.Component;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@Component
-
-@NoArgsConstructor
-@AllArgsConstructor
 @Getter
 @Setter
-
+@NoArgsConstructor
+@AllArgsConstructor
+@Component
+@Entity
+@Table(name = "Docentes")
 public class Docente {
-	private String legajo;
-	private String nombre;
-	private String apellido;
-	private String email;
-	private String telefono;
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	@Column(name = "legajo", nullable = false, unique = true)
+	private String legajo;
+	@Column(name = "nombre", nullable = false)
+	private String nombre;
+	@Column(name = "apelido", nullable = false)
+	private String apellido;
+	@Column(name = "email", nullable = false)
+	private String email;
+	@Column(name = "telefono", nullable = false)
+	private String telefono;
 	
 }
