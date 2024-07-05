964259ac705d7cb5750437b9feb001f2610d61cc
SOSA <SOSA@DESKTOP-V4PQRV0>
2024-07-05 19:16:06 -0300

    Agregando las Validaciones de Docente

--- tp4_VelizGuadalupe/src/main/java/ar/edu/unju/fi/controller/DocenteController.java
+++ tp4_VelizGuadalupe/src/main/java/ar/edu/unju/fi/controller/DocenteController.java
@@ -3,11 +3,13 @@ package ar.edu.unju.fi.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import ar.edu.unju.fi.dto.DocenteDTO;
 import ar.edu.unju.fi.service.DocenteService;
+import jakarta.validation.Valid;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -38,9 +40,12 @@ public class DocenteController {
 	}
 
 	@PostMapping("/guardar")
-	public String guardarDocente(@ModelAttribute("unDocente") DocenteDTO docenteDTO, Model model,
-			RedirectAttributes redirectAttributes) {
-		
+	public String guardarDocente(@Valid @ModelAttribute("unDocente") DocenteDTO docenteDTO, BindingResult result, Model model) {
+		if (result.hasErrors()) {
+			model.addAttribute("unDocente", docenteDTO);
+			model.addAttribute("edicion", false);
+			return "docentes/formDocente";
+		}
 		docenteService.saveDocente(docenteDTO);
 		return "redirect:/docentes/lista";
 	}
@@ -58,8 +63,12 @@ public class DocenteController {
 	}
 
 	@PostMapping("/modificar")
-	public String modificarDocente(@ModelAttribute("unDocente") DocenteDTO docenteDTO, Model model, RedirectAttributes redirectAttributes) {
-		
+	public String modificarDocente(@Valid @ModelAttribute("unDocente") DocenteDTO docenteDTO, BindingResult result, Model model) {
+		if (result.hasErrors()) {
+			model.addAttribute("unDocente", docenteDTO);
+			model.addAttribute("edicion", true);
+			return "docentes/formDocente";
+		}
 		docenteService.editDocente(docenteDTO);
 		return "redirect:/docentes/lista";
 	}
--- tp4_VelizGuadalupe/src/main/java/ar/edu/unju/fi/dto/DocenteDTO.java
+++ tp4_VelizGuadalupe/src/main/java/ar/edu/unju/fi/dto/DocenteDTO.java
@@ -1,6 +1,10 @@
 package ar.edu.unju.fi.dto;
 
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -13,9 +17,22 @@ import lombok.Setter;
 public class DocenteDTO {
 	
 	private Long id;
+	
+	@NotEmpty(message = "Debe ingresar un legajo.")
+	@Pattern(regexp = "^[a-zA-Z][0-9]{0,4}$", message = "Debe comenzar con una letra seguida de 4 números")
 	private String legajo;
+	
+	@NotEmpty(message = "Debe ingresar un nombre.")
 	private String nombre;
+	
+	@NotEmpty(message = "Debe ingresar un apellido.")
 	private String apellido;
+	
+	@NotEmpty(message = "Debe ingresar un email.")
+	@Email(message = "Debe ser un email válido")
 	private String email;
+	
+	@NotEmpty(message = "Debe ingresar un telefono.")
+	@Size(min=10,max=10, message="El telefono debe ser 388XXXXXXX.") 
 	private String telefono;
 }
