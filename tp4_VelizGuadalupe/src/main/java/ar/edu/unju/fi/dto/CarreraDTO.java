551cc193b13da1a37a37e262f3550a57f5f4479f
usuario <usuario@DESKTOP-2FH21K9>
2024-07-11 00:02:36 -0300

    correccion CarreraDTO

--- tp4_VelizGuadalupe/src/main/java/ar/edu/unju/fi/dto/CarreraDTO.java
+++ tp4_VelizGuadalupe/src/main/java/ar/edu/unju/fi/dto/CarreraDTO.java
@@ -3,6 +3,9 @@ package ar.edu.unju.fi.dto;
 import java.util.List;
 
 import ar.edu.unju.fi.model.Alumno;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -15,8 +18,13 @@ import lombok.Setter;
 public class CarreraDTO {
 	 
 	private Long id;
+	@NotEmpty(message = "Debe ingresar un codigo.")
 	private String codigo;
+	@NotEmpty(message = "Debe ingresar un nombre.")
 	private String nombre;
+	@NotEmpty(message = "Debe ingresar una cantidad.")
+	@Min(value = 1, message = "El curso debe ser al menos 1")
+	@Max(value = 7, message = "El curso no puede ser mayor que 7")
 	private int cantidadAnios;
 	private Estado estado;
 	
